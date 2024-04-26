48c982c43d1833522ca54aba3d60be9a5e79f44d
KIPASGTS <kitibt1@gmail.com>
2024-04-27 01:42:04 +0700

    variants update

--- clients/main.go
+++ clients/main.go
@@ -3,11 +3,12 @@ package clients
 import (
 	log "github.com/codecat/go-libs/log"
 	enet "github.com/eikarna/gotops"
+	pkt "github.com/eikarna/gotps/packet"
 )
 
 func OnConnect(peer enet.Peer, host enet.Host) {
 	log.Info("New Client Connected %s", peer.GetAddress().String())
-
+	pkt.SendPacket(peer, 1, "") //hello response
 }
 
 func OnDisConnect(peer enet.Peer, host enet.Host) {
--- functions/main.go
+++ functions/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func SendWorldMenu(peer enet.Peer) {
-	variant.
+
 }
 
 //variants
